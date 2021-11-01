397d4b1274111619d872c323d79abbec4f6533d9
zivserphos <zivfromisrael@gmail.com>
2021-11-01 18:21:23 +0200

    boilerplate

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -38,8 +38,7 @@ module.exports = {
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
-      template: paths.src + '/template.html', // template file
+      template: paths.src + '/index.html', // template file
       filename: 'index.html', // output file
     }),
   ],
